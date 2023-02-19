1ec214aa6b604caa7fc0303e3f0b53fd16825915
Lona <k.inagaki1001@gmail.com>
2023-02-19 15:41:28 +0900

    ßverison

--- index.js
+++ index.js
@@ -22,5 +22,51 @@ function getCSV(path) {
     return getProcessedCSV(getLoadingCSV(path));
 }
 
-module.exports = getLoadingCSV, getProcessedCSV, getCSV;
-module.exports.default = getLoadingCSV, getProcessedCSV, getCSV;
+function toCSV(array) {
+    let csv = "",
+        line,
+        txt;
+    if (typeof array != "object") {
+        return null;
+    }
+    if (typeof array[0] != "object") {
+        return null;
+    }
+    for (num_1 in array) {
+        line = array[num_1];
+        for (num_2 in line) {
+            txt = line[num_2];
+            csv += txt + ",";
+        }
+        csv = csv.substring(0, csv.length - 1) + "\n";
+    }
+    csv = csv.substring(0, csv.length - 1);
+    return csv;
+}
+
+function writeCSV(path, str, opt = 0) {
+    switch (opt) {
+        case 0:
+            // 新規書き込み/上書き デフォルト
+            fs.writeFileSync(path, str);
+            break;
+        case 1:
+            // 追記
+            fs.appendFileSync(path, str);
+            break;
+        default:
+            console.log("\u001b[31mError: Third option is 0 or 1!!\u001b[0m");
+            return false;
+    }
+}
+
+function setCSV(path, array, opt = 0) {
+    writeCSV(path, toCSV(array), opt);
+}
+
+exports.getLoadingCSV = getLoadingCSV;
+exports.getProcessedCSV = getProcessedCSV;
+exports.getCSV = getCSV;
+exports.toCSV = toCSV;
+exports.writeCSV = writeCSV;
+exports.setCSV = setCSV;
