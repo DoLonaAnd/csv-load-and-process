11ab8855e2830cb3a4b88759732efeb276bb6000
Lona <k.inagaki1001@gmail.com>
2023-02-19 12:25:35 +0900

    modify getProcessedCSV() method.

--- index.js
+++ index.js
@@ -7,31 +7,10 @@ function getLoadingCSV(path) {
 function getProcessedCSV(str) {
     let arrayCSV = [],
         lines = [],
-        line = [],
-        start = 0,
-        count = 0;
-    for (let i = 0; i < str.length; i++) {
-        count++;
-        if (str.substr(i, 1) == "\n") {
-            lines.push(str.substr(start, count - 1));
-            start = i + 1;
-            count = 0;
-        }
-    }
-    lines.push(str.substr(start));
-    for (let i = 0; i < lines.length; i++) {
-        start = 0;
-        count = 0;
         line = [];
-        for (let j = 0; j < lines[i].length; j++) {
-            count++;
-            if (lines[i].substr(j, 1) == ",") {
-                line.push(lines[i].substr(start, count - 1));
-                start = j + 1;
-                count = 0;
-            }
-        }
-        line.push(lines[i].substr(start, count));
+    lines = str.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+        line = lines[i].split(",");
         if (line != "") {
             arrayCSV.push(line);
         }
@@ -42,7 +21,6 @@ function getProcessedCSV(str) {
 function getCSV(path) {
     return getProcessedCSV(getLoadingCSV(path));
 }
-
 
 module.exports = getLoadingCSV, getProcessedCSV, getCSV;
 module.exports.default = getLoadingCSV, getProcessedCSV, getCSV;
